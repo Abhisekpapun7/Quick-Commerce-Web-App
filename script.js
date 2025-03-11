11554a3e4154366a39e9cce6fd81e12062d1080e
Abhisekpapun7 <your-correct-email@example.com>
2025-03-11 21:42:09 +0530

    adding banner for ad section

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ categories.forEach(category => {
         this.classList.add('active');
     });
 });
+
+// logic for slideshow bar
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+    slides.forEach(slide => slide.style.display = "none"); 
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 } 
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Auto change every 3 seconds
+}
+
+function changeSlide(n) {
+    slideIndex += n - 1;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    slides.forEach(slide => slide.style.display = "none"); 
+    slides[slideIndex].style.display = "block";
+}
+
+// Initialize slideshow
+showSlides();
